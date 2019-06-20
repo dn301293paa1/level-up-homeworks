701a09d17129ecb1b4eff7d38e74efd29991d2ab
dn301293paa1 <dn301293paa1@gmail.com>
2019-06-21 00:48:31 +0300

    CSS HW 1

---
+++ public/views/javascript/homework-3/js/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+// Unit link - https://learn.javascript.ru/operators#tasks
+
+// Task 1
+// да алерт выведится , так как это строка с 0  и не пустая строка , это означает истину 
+
+
+// Task 2
+let officialNameJs= prompt("Каково «официальное» название JavaScript?");
+ answer= (officialNameJs =='ECMAScript') ?  'Верно' : 'Не знаете? «ECMAScript»!';
+ console.log(answer)
+
+
+// Task 3
+let num =  prompt('Введите число', 0);
+answer =(num>0) ? 1 : (num<0) ? -1 : 0;
+console.log(answer)
+
+
+// task 4 
+var userName = prompt('Кто пришёл?','');
+
+if (userName == 'Админ') {
+
+  var pass = prompt('Пароль?','');
+
+  if (pass == 'Чёрный Властелин') {
+    console.log( 'Добро пожаловать!' );
+  } else if (pass == null) { 
+    console.log( 'Вход отменён' );
+  } else {
+    console.log( 'Пароль неверен' );
+  }
+
+} else if (userName == null) { 
+  console.log( 'Вход отменён' );
+
+} else {
+
+  console.log( 'Я вас не знаю' );
+
+}
+
+// Task 5
+var result = (a+b<4) ? 'Мало' : 'Много'
+console.log(result)
+
+
+// Task 6
+var message =(login== 'Вася') ? 'Привет' : (login=='Директор') ? 'Здравствуйте' : (login=='') ? 'Нет логина' : ''
+console.log(message)
+
+// Unit link - https://learn.javascript.ru/logical-ops
+
+// Task 1
+
+// выведит 2, это значение, которое  true.
+
+// Task 2
+
+
+// Task 3
+
+// выведит null, это первое  значение false
+
+ // Task 4
+
+
+ // Task 5
+
+ // выведит 3. так как при услвовие и если елемент истина то выводит последний елемент 
+
+
+//Task 6
+
+if (age >= 14 && age <= 90);
+
