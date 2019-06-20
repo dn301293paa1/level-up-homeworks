98a5022ecaaf6039138ae04dc5745b3b797a383f
dn301293paa1 <dn301293paa1@gmail.com>
2019-06-20 22:20:55 +0300

    CSS HW 1

---
+++ public/views/javascript/homework-1/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Unit link - https://learn.javascript.ru/variables#tasks
+
+
+// Task 1
+let admin , name;
+name='Василий';
+admin= name;
+console.log(admin);
+
+// Unit link - https://learn.javascript.ru/variable-names#tasks
+
+//  Task 2
+let ourPlanet = 'Земля';
+let userName = 'Andrii';
+
+// Additional task 1
+
+let fps;
+let connectingSign;
+let documentView;
+let showLastPagePagination;
+
