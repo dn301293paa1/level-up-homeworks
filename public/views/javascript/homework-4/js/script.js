e1481bacfc8550a88399338051b4d05875d2a546
dn301293paa1 <dn301293paa1@gmail.com>
2019-06-21 23:58:18 +0300

    js-4

--- public/views/javascript/homework-4/js/script.js
+++ public/views/javascript/homework-4/js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
-// Unit link - https://learn.javascript.ru/operators#tasks
+// Unit link - https://learn.javascript.ru/function-basics
+
+//  Task 1 
+//  отличий не будет
+
+//Task 2
+function checkAge (age) {
+ return (age>18) ?  true :  (age<18) ? confirm('Родители разрешили?'): false
+ }
+ 
+  console.log(checkAge(1));
 
 
