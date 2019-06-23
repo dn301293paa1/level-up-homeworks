724a4304e4cb6d6c1c0a372a3571d0e20a89b348
dn301293paa1 <dn301293paa1@gmail.com>
2019-06-23 14:29:01 +0300

    HW-4

--- public/views/javascript/homework-4/js/script.js
+++ public/views/javascript/homework-4/js/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
+// Unit link- https://learn.javascript.ru/while-for
+
+//  Task 1
+// выведит последнее значение 1 ,так как в цыкле используеться декремент и на 0 сработает false 
+
+//  Task 2
+
+// выведит от 1 до 4 Префиксный, сначала увеличивает потом проверяет
+// выведит от 1 до 5 постфикс увеличивает.затем возвращает старое значение
+
+//  Task 3
+// от 0 до 4 в обох случаях, так как сначала происходит проверка, а потом увелечение
+
+//  Task 4
+var i;
+for(i=2;i<=10;i++) {
+ if(i%2===0){
+ console.log(i);
+ }
+}
+
+//  Task 5
+var i=0;
+while (i<3){
+console.log( "номер " + i + "!" );
+i++
+}
+
+// Task 6
+
+
+
 // Unit link - https://learn.javascript.ru/function-basics
 
 //  Task 1 
@@ -13,7 +45,6 @@ function checkAge (age) {
   console.log(checkAge(1));
 
 //Task 3
-
 function min (a,b) {
 return (a<b) ? a : (a>b) ? b : a
 }
