da979235ac47e4da0f77c1387324121d3da1dbfb
dn301293paa1 <dn301293paa1@gmail.com>
2019-07-08 00:14:53 +0300

    homeWor7

--- public/views/javascript/homework-7/js/script.js
+++ public/views/javascript/homework-7/js/script.js
@@ -2,9 +2,48 @@
 
 //Unit link https://learn.javascript.ru/object
 // Task 1
-const user = {};
-user.name = 'Вася';
-user.surname = 'Петров';
-user.name = 'Сергей';
-delete user.name;
-console.log(user);
+// const user = {};
+// user.name = 'Вася';
+// user.surname = 'Петров';
+// user.name = 'Сергей';
+// delete user.name;
+// console.log(user);
+
+// //Unit link https://learn.javascript.ru/object-for-inhttps://learn.javascript.ru/object-for-in
+// // Task 1
+// function isEmpty(obj) {
+//   let count = 0;
+//   for (let key in obj) {
+//     count++;
+//   }
+//   if (count == 0) {
+//     return false;
+//   }
+//   return true;
+// }
+// var menu = {};
+// console.log(isEmpty());
+// // Task 2
+// function isEmpty(obj) {
+//   let sum = 0;
+//   for (let key in obj) {
+//     sum = sum + obj[key];
+//   }
+//   return sum;
+// }
+// console.log(isEmpty(salaries));
+
+// Task 3
+function isEmpty(obj) {
+  let sum = 0;
+  let result;
+  for (let key in obj) {
+    if (obj[key] > sum) {
+      result = obj[key];
+      sum = result;
+    } else {
+      result = sum;
+    }
+  }
+  return result;
+}
