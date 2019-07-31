b640a8f781dac2484a8ce4d88f5c4fea883fe0a3
dn301293paa1 <dn301293paa1@gmail.com>
2019-07-31 23:15:41 +0300

    HW Array

--- public/views/javascript/homeWork-8/js/script.js
+++ public/views/javascript/homeWork-8/js/script.js
@@ -1,24 +1,128 @@
 'use strict';
 
-// Unit link - https://learn.javascript.ru/variables#tasks
+Unit link https://learn.javascript.ru/array
+
+Task1
+
+function array(arr) {
+  let lastElem = arr[arr.length - 1];
+  return lastElem;
+}
+array();
+
+// Task2
+
+arr.push('Компьютер');
+
+// Task3
+
+var styles = ['Джаз', 'Блюз'];
+styles.push('Рок-н-Ролл');
+styles[styles.length - 2] = 'Классика';
+console.log(styles.shift());
+styles.unshift('Рэп', 'Регги');
+
+// Task4
+
+function randomArray(arr) {
+  let elem = Math.floor(Math.random() * arr.length);
+  console.log(arr[elem]);
+}
+
+//task 5
+let arr = [];
+while (true) {
+  let elem = prompt('ведите число');
+  if (elem == null || elem == '' || elem == isNaN) break;
+  arr.push(+elem);
+}
+
+let i;
+let sum = 0;
+for (i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+console.log(sum);
+
+//task 6
+var arr = [1, 2, 3];
+var arr2 = arr; // присвоили arr2  ссылку масива arr
+arr2[0] = 5; // поменяли по ссылке 0 елемент масива ссылки
+console.log(arr[0], arr2[0]); // будет в двух случаях 5 так как ссылка у двух массивов одинаковая
+
+// unit https://learn.javascript.ru/array-methods#tasks
+//task  Функция removeClass
+var obj = {
+  className: 'open menu menu menu'
+};
+
+function removeClass(obj, cls) {
+  var item = obj.className.split(' ');
+  for (var i = 0; i < item.length; i++) {
+    if (item[i] == cls) {
+      item.splice(i, 1);
+      i--;
+    }
+  }
+  obj.className = item.join(' ');
+}
+
+//task Добавить класс в строку
+
+var obj = {
+  className: 'open menu menu'
+};
+function addClass(obj, cls) {
+  let item = obj.className.split(' ');
+  for (let i = 0; i < item.length; i++) {
+    if (item[i] != cls) {
+      item.push(cls);
+    }
+    break;
+  }
+  obj.className = item.join(' ');
+}
+
+//task  Фильтрация массива "на месте"
+let arr = [5, 3, 8, 1];
+
+function filterRangeInPlace(arr, a, b) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      continue;
+    } else {
+      arr.splice(i, 1);
+    }
+  }
+}
+filterRangeInPlace(arr, 1, 5);
+
+// task cкопировать и отсортировать массив
+arr.sort(function(a, b) {
+  return 0.5 - Math.random();
+});
+console.log(arr);
+
+//Task Сортировать в обратном порядке
+var arr = [5, 2, 1, -10, 8];
+arr.sort(function(a, b) {
+  return b - a;
+});
+console.log(arr);
+
+// task
+
+var arr = ['HTML', 'JavaScript', 'CSS'];
+let arrSorted = arr.slice().sort();
 
 
-// Task 1
-let admin , name;
-name='Василий';
-admin= name;
-console.log(admin);
+//unit link  https://learn.javascript.ru/array-iteration#map
+//task 1
 
-// Unit link - https://learn.javascript.ru/variable-names#tasks
+var arrLength= arr.map(function(names){
+  return names.length
+})
 
-//  Task 2
-let ourPlanet = 'Земля';
-let userName = 'Andrii';
+console.log(arrLength)
 
-// Additional task 1
-
-let fps;
-let connectingSign;
-let documentView;
-let showLastPagePagination;
 
--- public/views/javascript/homework-7/js/script.js
+++ public/views/javascript/homework-7/js/script.js
@@ -1,72 +1,132 @@
-'use strict';
+// 'use strict';
 
-// Unit link https://learn.javascript.ru/array
+// // Unit link https://learn.javascript.ru/array
 
-// Task 1
+// // Task 1
 
-// function getLastArray(arr) {
-//   return arr[arr.length - 1];
+// // function getLastArray(arr) {
+// //   return arr[arr.length - 1];
+// // }
+
+// // // Task 2
+
+// // function addArray(arr, elem) {
+// //   arr.push(elem);
+// // }
+// // addArray(arr, 'Компьютер');
+
+// // // Task 3
+
+// // var styles = ['Джаз', 'Блюз'];
+// // styles.push('Рок-н-Ролл');
+// // styles[styles.length - 2] = 'Классика';
+// // console.log(styles.shift());
+// // styles.unshift('Рэп', 'Регги');
+
+// // task 4
+// let a = [];
+// function generateRandom() {
+//   return 10 * Math.round(Math.random());
 // }
 
-// // Task 2
-
-// function addArray(arr, elem) {
-//   arr.push(elem);
+// function generateArray() {
+//   for (let i = 0; i < 100; i++) {
+//     a.push(generateRandom());
+//   }
 // }
-// addArray(arr, 'Компьютер');
 
-// // Task 3
+// console.log(a);
+// // Task 5
+// var arr = [1, 2, 3]; //создали массив
+// var arr2 = arr; // в arr2  присвоили ссылку arr
+// arr2[0] = 5; // присвоили arr2[0] пятерку, поскольку ссылка на хранения данных одна в arr[0]также изменилисось значение на 5, далее в выводе ,мы можем это увидеть
+// console.log(arr[0]);
+// console.log(arr2[0]); //
 
-// var styles = ['Джаз', 'Блюз'];
-// styles.push('Рок-н-Ролл');
-// styles[styles.length - 2] = 'Классика';
-// console.log(styles.shift());
-// styles.unshift('Рэп', 'Регги');
+// // Task 7
 
-// task 4
-let a = [];
-function generateRandom() {
-  return 10 * Math.round(Math.random());
+// function find(arr, value) {
+//   for (var i = 0; i < arr.length; i += 1) {
+//     if (arr[i] === value) {
+//       let num = value;
+//       return num;
+//     }
+//   }
+//   return -1;
+// }
+
+// //Task 8
+// var arr = ['Яблоко', 'Апельсин', 'Груша'];
+// arr = [2, 6, 3, 4, 44, 141];
+
+// function filterRange1(arr, a, b) {
+//   let arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= a && arr[i] <= b) {
+//       arr2.push(arr[i]);
+//     }
+//   }
+//   return arr2;
+// }
+// console.log(filterRange1(arr, 2, 4));
+// console.log(arr2);
+
+// let array = [-2, -1, 1, 2];
+
+// function getMaxSubSum(arr) {
+//   let maxSum = 0;
+//   let elem = 0;
+//   let i;
+//   for (i = 0; i < arr.length; i++) {
+//     elem = elem + arr[i];
+//     maxSum = Math.max(maxSum, elem);
+//     if (elem < 0) {
+//       elem = 0;
+//     }
+//   }
+//   return maxSum;
+// }
+// console.log(getMaxSubSum(array));
+
+// function camelize(str) {
+//   let arr;
+//   arr = str.split('-');
+//   let i;
+//   for (i = 0; i < arr; i++) {
+//     var str1 = str1.charAt(0).toUpperCase().str1[i];
+//   }
+//   return arr;
+// }
+
+// console.log(camelize('webkit-transition'));
+
+// //task1
+// arr.sort(function(a, b) {
+//   return b - a;
+// }); // 8, 5, 2, 1, -10
+// console.log(arr);
+
+function baz() {
+  // стек вызовов: `baz`
+  // поэтому наша точка вызова — глобальная область видимости
+
+  console.log('baz');
+  bar(); // <-- точка вызова для `bar`
 }
 
-function generateArray() {
-  for (let i = 0; i < 100; i++) {
-    a.push(generateRandom());
-  }
+function bar() {
+  // стек вызовов: `baz` -> `bar`
+  // поэтому наша точка вызова в `baz`
+
+  console.log('bar');
+  foo(); // <-- точка вызова для `foo`
 }
 
-console.log(a);
-// Task 5
-var arr = [1, 2, 3]; //создали массив
-var arr2 = arr; // в arr2  присвоили ссылку arr
-arr2[0] = 5; // присвоили arr2[0] пятерку, поскольку ссылка на хранения данных одна в arr[0]также изменилисось значение на 5, далее в выводе ,мы можем это увидеть
-console.log(arr[0]);
-console.log(arr2[0]); //
+function foo() {
+  // стек вызовов: `baz` -> `bar` -> `foo`
+  // поэтому наша точка вызова в `bar`
 
-// Task 7
-
-function find(arr, value) {
-  for (var i = 0; i < arr.length; i += 1) {
-    if (arr[i] === value) {
-      let num = value;
-      return num;
-    }
-  }
-  return -1;
+  console.log('foo');
 }
 
-//Task 8
-var arr = ['Яблоко', 'Апельсин', 'Груша'];
-arr = [2, 6, 3, 4, 44, 141];
-
-function filterRange1(arr, a, b) {
-  let arr2 = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= a && arr[i] <= b) {
-      arr2.push(arr[i]);
-    }
-  }
-  return arr2;
-}
-console.log(filterRange1(arr, 2, 4));
-console.log(arr2);
+baz(); // <-- точка вызова для `baz`
